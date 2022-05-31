e27ddab184bd5584a6d347e4232daaefc46a421b
tnoonan <tnoonan88@gmail.com>
2022-05-31 16:20:15 -0400

    updated schemas directory, built out typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,35 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, { user }) => {  
+            return User.findOne({_id: user._id});
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });         
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args } },
+                    { new: true, runValidators: true }
+                  );
+            }
+        },
+        
+    }
+}
